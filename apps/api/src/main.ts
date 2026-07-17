cfe05a40c5c2406d01531c160a75968e0440d2a2
Bhupendra Watti <bhupendrawatti24@gmail.com>
2026-07-18 01:41:55 +0530

    Copy compiled .next from active apisales nodejs apps/web

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -67,8 +67,8 @@ function copyLogs() {
             encoding: 'utf8'
           });
           
-          // Copy compiled .next directory from successful Hostinger build source folder of apisales to active sales nodejs folder
-          const srcNext = '/home/u163598660/domains/apisales.granthinfotech.in/public_html/.builds/source/repository/apps/web/.next';
+          // Copy compiled .next directory from apisales nodejs apps/web/.next to active sales nodejs folder
+          const srcNext = '/home/u163598660/domains/apisales.granthinfotech.in/nodejs/apps/web/.next';
           const destNext = '/home/u163598660/domains/sales.granthinfotech.in/nodejs/apps/web/.next';
           
           let buildLog = '';
@@ -76,7 +76,7 @@ function copyLogs() {
             console.log(`[Diagnostic] Copying compiled .next from ${srcNext} to ${destNext}...`);
             execSync(`rm -rf "${destNext}"`);
             execSync(`cp -r "${srcNext}" "${destNext}"`);
-            buildLog = `Successfully copied compiled .next folder from source/repository!`;
+            buildLog = `Successfully copied compiled .next folder from apisales active nodejs apps/web!`;
           } else {
             buildLog = `Compiled .next folder not found at: ${srcNext}`;
           }
